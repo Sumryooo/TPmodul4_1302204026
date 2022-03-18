b6629195198d87faa927de7cd8358393c4a430d6
sumryooo <adha.surya2002@gmail.com>
2022-03-18 16:49:51 +0700

    Menambahkan Class  DataGeneric

---
+++ DataGeneric.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TPmod4_1302204026
+    {
+        public class DataGeneric<T>
+        {
+            private T data;
+
+            public DataGeneric(T data)
+            {
+                this.data = data;
+            }
+
+            public void printData()
+            {
+                Console.WriteLine("Data yang tersimpan adalah : " + this.data);
+            }
+
+        }
+    }
--- Program.cs
+++ Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+
+namespace TPmod4_1302204026
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            DataGeneric<String> c = new DataGeneric<String>("1302204026"); ;
+            c.printData();
+
+        }
+    }
+
+}
